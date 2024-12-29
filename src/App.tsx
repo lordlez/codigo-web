f76b96381c4075fa1b908665442faabf832b38b9
Lorenzo Lezcano <lorenzodlezcano@gmail.com>
2024-12-28 21:26:49 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Estos son los componentes que van a componer la página
+// Falta crearlos
+
+function App() {
+  return (
+    <div>
+      {/* <Navbar />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
